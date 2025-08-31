aeb6995367a2f723f049fafcadaab2d1435e1c2b
Khanh Nguyen <nguyen_j6@denison.edu>
2025-08-31 11:30:16 -0400

    LC-maximum-69-number [Runtime: 0 ms; Memory: 17.6 MB]

--- math/maximum-69-number.py
+++ math/maximum-69-number.py
@@ -1,12 +1,3 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        num = str(num)
-        new = ''
-        changed = False
-        for i in range(len(num)):
-            if num[i] == '9':
-                new += num[i]
-            elif num[i] != '9' and changed == False:
-                new += '9'
-                changed = True
-        return int(new)
+        return int(str(num).replace('6', '9', 1))
