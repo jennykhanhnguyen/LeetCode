e4523fdf6695e8b82588d0ff5db2b612b0a39b28
Khanh Nguyen <nguyen_j6@denison.edu>
2025-09-10 14:27:45 -0400

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 0 ms; Memory: 18.9 MB]

--- greedy/best-time-to-buy-and-sell-stock-ii.py
+++ greedy/best-time-to-buy-and-sell-stock-ii.py
@@ -3,8 +3,5 @@ class Solution:
         total = 0
         for i in range(len(prices)-1):
             if prices[i] < prices[i+1]:
-                if i+2 < len(prices):
-                    total += max(prices[i+1]-prices[i], prices[i+2]-prices[i+1])
-                else:
                     total += prices[i+1]-prices[i]
         return total
