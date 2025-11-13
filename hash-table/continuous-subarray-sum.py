9891c7696bb02a75f523d5240d629309a768ef48
Jenny Nguyen <nguyen_j6@denison.edu>
2025-11-13 09:06:49 -0500

    LC-continuous-subarray-sum [Runtime: 43 ms; Memory: 38.1 MB]

--- hash-table/continuous-subarray-sum.py
+++ hash-table/continuous-subarray-sum.py
@@ -1,19 +1,16 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         res = 0
-        dic = {0:1}
+        dic = {0:-1}
         n = len(nums)
-        summ = 0
-        for j in range(0,n):
-            summ += nums[j]
-            if summ%k not in dic:
-                dic[summ%k] = 0
-            if summ%k in dic:
-                res += dic[summ%k]
-            dic[summ%k]+=1
-            print(summ)
-            print(summ//k)
-            print(dic)
-            print(res)
-        print(dic)
-        return res>0
+        if n <2:
+            return False
+        mod = 0
+        for j in range(n):
+            mod = (mod + nums[j])%k
+            if mod in dic:
+                if j - dic[mod] > 1:
+                    return True
+            else:
+                dic[mod] = j
+        return False
