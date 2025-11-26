f7d1ea2f1336539b7c3b733132bbca4a38ee78d8
Jenny Nguyen <nguyen_j6@denison.edu>
2025-11-26 13:52:35 -0500

    LC-shortest-path-in-binary-matrix [Runtime: N/A; Memory: N/A]

--- array/shortest-path-in-binary-matrix.py
+++ array/shortest-path-in-binary-matrix.py
@@ -4,24 +4,24 @@ class Solution:
         n = len(grid)
 
         # edge case
-        if grid[0][0] == 0 and n == 1:
+        if grid[0][0] == 0 and n == 1 and len(grid[0]) == 1:
             return 1
-        if grid[0][0] != 0 or grid[n-1][n-1] != 0:
+        elif grid[0][0] != 0 or grid[n-1][n-1] != 0:
             return -1
-        
+        print('a')
         visited = [[0]*n]*n
         queue = deque()
         queue.append((0,0))
         res = 1
-        directionx = [1,1,0]
-        directiony = [1,0,1]
+        directionx = [-1,-1,-1,0,0,1,1,1]
+        directiony = [-1,0,1,-1,1,-1,0,1]
         while queue:           
             res += 1
             # print(0,x,y,res)
             size = len(queue)
             for i in range(size):
                 x,y = queue.popleft()
-                for i in range(3):
+                for i in range(8):
                     newx = x + directionx[i]
                     newy = y + directiony[i]
                     if newx >= 0 and newx <= n-1 and newy >= 0 and newy <= n-1:
