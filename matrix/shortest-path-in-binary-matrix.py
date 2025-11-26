e5d1047cc35438b1df40dc7daaead88dde1f2b4f
Jenny Nguyen <nguyen_j6@denison.edu>
2025-11-26 13:42:36 -0500

    LC-shortest-path-in-binary-matrix [Runtime: N/A; Memory: N/A]

--- matrix/shortest-path-in-binary-matrix.py
+++ matrix/shortest-path-in-binary-matrix.py
@@ -2,7 +2,7 @@ from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        
+
         # edge case
         if grid[0][0] != 0 or grid[n-1][n-1] != 0:
             return -1
@@ -13,20 +13,25 @@ class Solution:
         res = 1
         directionx = [1,1,0]
         directiony = [1,0,1]
-        while queue:
-            x,y = queue.popleft()
+        while queue:           
             res += 1
-            print(0,x,y)
-            for i in range(3):
-                newx = x + directionx[i]
-                newy = y + directiony[i]
-                if newx >= 0 and newx <= n-1 and newy >= 0 and newy <= n-1:
-                    if grid[newx][newy] == 0 and visited[newx][newy] == 0:
-                        queue.append((newx,newy))
-                        visited[newx][newy] == 1
-                        print(1,newx, newy)
-                    if newx == n-1 and newy == n-1:
-                        return res
+            # print(0,x,y,res)
+            size = len(queue)
+            for i in range(size):
+                x,y = queue.popleft()
+                for i in range(3):
+                    newx = x + directionx[i]
+                    newy = y + directiony[i]
+                    if newx >= 0 and newx <= n-1 and newy >= 0 and newy <= n-1:
+                        if grid[newx][newy] == 0 and visited[newx][newy] == 0:
+                            queue.append((newx,newy))
+                            visited[newx][newy] == 1
+                            # print(1,newx, newy)
+                        if newx == n-1 and newy == n-1:
+                            return res
                     
-        # print(x,y)
-        return 0
+        return -1
+
+# [[0,0,0]
+# ,[0,1,0]
+# ,[0,0,0]]
