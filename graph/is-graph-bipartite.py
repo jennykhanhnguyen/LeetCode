9c1c8d04055ae496221da884f724a131163b5393
Jenny Nguyen <nguyen_j6@denison.edu>
2025-12-06 19:24:10 -0500

    LC-is-graph-bipartite [Runtime: 123 ms; Memory: 18.5 MB]

--- graph/is-graph-bipartite.py
+++ graph/is-graph-bipartite.py
@@ -4,28 +4,31 @@ class Solution:
         visited = [0]*len(graph)
         red = set()
         blue = set()
-        red.add(0)
         queue = deque()
-        queue.append(0)
-        while queue:
-            size = len(queue)
-            for k in range(size):
-                node = queue.popleft()
-                visited[node] = 1
-                if node in red:
-                    for z in range(len(graph[node])):      
-                        if graph[node][z] in red: 
-                            return False           
-                        if visited[graph[node][z]] == 0:
-                            queue.append(graph[node][z])
-                            blue.add(graph[node][z])
-                            # visited[graph[node][z]] = 1
-                elif node in blue:
-                    for z in range(len(graph[node])):   
-                        if graph[node][z] in blue: 
-                            return False       
-                        if visited[graph[node][z]] == 0:
-                            queue.append(graph[node][z])
-                            red.add(graph[node][z])
-                            # visited[graph[node][z]] = 1
+        for i in range(len(graph)):
+            if visited[i] == 0:
+                queue.append(i)
+                red.add(i)
+                while queue:
+                    size = len(queue)
+                    for k in range(size):
+                        node = queue.popleft()
+                        visited[node] = 1
+                        if node in red:
+                            for z in range(len(graph[node])):      
+                                if graph[node][z] in red: 
+                                    return False           
+                                if visited[graph[node][z]] == 0:
+                                    queue.append(graph[node][z])
+                                    blue.add(graph[node][z])
+                                    # visited[graph[node][z]] = 1
+                        elif node in blue:
+                            for z in range(len(graph[node])):   
+                                if graph[node][z] in blue: 
+                                    return False       
+                                if visited[graph[node][z]] == 0:
+                                    queue.append(graph[node][z])
+                                    red.add(graph[node][z])
+                                    # visited[graph[node][z]] = 1
+        print(red, blue)
         return True
