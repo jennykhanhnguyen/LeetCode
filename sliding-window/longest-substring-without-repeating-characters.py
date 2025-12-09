4600f6146d0ecbbffdfa4ad159b1637df517acdf
Jenny Nguyen <nguyen_j6@denison.edu>
2025-12-08 23:20:25 -0500

    LC-longest-substring-without-repeating-characters [Runtime: 25 ms; Memory: 13.3 MB]

--- sliding-window/longest-substring-without-repeating-characters.py
+++ sliding-window/longest-substring-without-repeating-characters.py
@@ -1,12 +1,20 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char = set()
         left = 0
         res = 0
-        chars = set()
         for right in range(len(s)):
-            while s[right] in chars:
-                chars.remove(s[left])
-                left += 1      
-            chars.add(s[right])
-            res = max(res, right-left+1)
+            if s[right] in char:
+                while s[right] in char:
+                    char.remove(s[left])
+                    left += 1
+                char.add(s[right])
+
+            elif s[right] not in char:
+                char.add(s[right])
+            res = max(res, right-left +1)
         return res
