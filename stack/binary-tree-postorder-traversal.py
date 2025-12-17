3a2a357c3f45bb84cf2885c2c3434bc7c7102494
Jenny Nguyen <nguyen_j6@denison.edu>
2025-12-16 22:52:28 -0500

    LC-binary-tree-postorder-traversal [Runtime: 0 ms; Memory: 12.3 MB]

---
+++ stack/binary-tree-postorder-traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        res = []
+        if not root:
+            return []
+        def dfs(node):
+            if node.left:
+                dfs(node.left)
+            if node.right:
+                dfs(node.right)
+            res.append(node.val)
+        dfs(root)
+        return res
