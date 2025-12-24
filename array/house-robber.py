3a0a4653487b904305fb32d5dbdaf485e79f08f0
Jenny Nguyen <nguyen_j6@denison.edu>
2025-12-23 21:02:03 -0500

    LC-house-robber [Runtime: N/A; Memory: N/A]

---
+++ array/house-robber.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return max(nums[0], nums[1])
+        dp = [0]*(len(nums))
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(len(nums)):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        return dp[len(nums)-1]
