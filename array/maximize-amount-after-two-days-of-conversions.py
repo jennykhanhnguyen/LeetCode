a6857c9e84a8062b70a41fb9dcc2afb404818145
Jenny Nguyen <nguyen_j6@denison.edu>
2025-12-27 17:11:01 -0500

    LC-maximize-amount-after-two-days-of-conversions [Runtime: 5 ms; Memory: 17.8 MB]

--- array/maximize-amount-after-two-days-of-conversions.py
+++ array/maximize-amount-after-two-days-of-conversions.py
@@ -22,7 +22,7 @@ class Solution:
                     q.append(nxt)
         return factor
 
-    def maximizeAmount(self, initialCurrency: str,
+    def maxAmount(self, initialCurrency: str,
                        pairs1: List[List[str]], rates1: List[float],
                        pairs2: List[List[str]], rates2: List[float]) -> float:
 
