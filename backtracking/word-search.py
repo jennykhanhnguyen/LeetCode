601cfaf6eeee4ca3ae1c14e2623db591faf818fd
Jenny Nguyen <nguyen_j6@denison.edu>
2025-12-27 17:45:16 -0500

    LC-word-search [Runtime: 5847 ms; Memory: 12.5 MB]

---
+++ backtracking/word-search.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        rows, cols = len(board), len(board[0])
+
+        def dfs(r, c, i):
+            # i = index in word we are matching
+
+            if i == len(word):
+                return True
+
+            if (r < 0 or r >= rows or
+                c < 0 or c >= cols or
+                board[r][c] != word[i]):
+                return False
+
+            # mark visited
+            temp = board[r][c]
+            board[r][c] = '#'
+
+            found = (
+                dfs(r+1, c, i+1) or
+                dfs(r-1, c, i+1) or
+                dfs(r, c+1, i+1) or
+                dfs(r, c-1, i+1)
+            )
+
+            # backtrack
+            board[r][c] = temp
+            return found
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+
+        return False
