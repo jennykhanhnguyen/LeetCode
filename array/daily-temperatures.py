becc1750895b8b297c4bef10ea1b2c5b5d73ecc7
Jenny Nguyen <nguyen_j6@denison.edu>
2026-01-07 02:28:44 -0500

    LC-daily-temperatures [Runtime: 187 ms; Memory: 26.9 MB]

--- array/daily-temperatures.py
+++ array/daily-temperatures.py
@@ -4,17 +4,12 @@ class Solution(object):
         :type temperatures: List[int]
         :rtype: List[int]
         """
-        n = len(temperatures)
-        res = []
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
 
-        for i in range(n):
-            count = 1
-            j = i + 1
-            while j < n:
-                if temperatures[j] > temperatures[i]:
-                    break
-                j += 1
-                count += 1
-            count = 0 if j == n else count
-            res.append(count)
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
         return res
