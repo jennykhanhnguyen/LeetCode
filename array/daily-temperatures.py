0207a8962fc01f64e8ed8758c33a97f3b40f2cbd
Jenny Nguyen <nguyen_j6@denison.edu>
2026-01-07 02:27:42 -0500

    LC-daily-temperatures [Runtime: N/A; Memory: N/A]

---
+++ array/daily-temperatures.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        n = len(temperatures)
+        res = []
+
+        for i in range(n):
+            count = 1
+            j = i + 1
+            while j < n:
+                if temperatures[j] > temperatures[i]:
+                    break
+                j += 1
+                count += 1
+            count = 0 if j == n else count
+            res.append(count)
+        return res
