82496161fe6fc3a1df4fcbb8e6dae0f0aa797e9f
Jenny Nguyen <nguyen_j6@denison.edu>
2026-01-19 18:28:54 -0500

    LC-find-largest-value-in-each-tree-row [Runtime: N/A; Memory: N/A]

--- tree/find-largest-value-in-each-tree-row.py
+++ tree/find-largest-value-in-each-tree-row.py
@@ -11,6 +11,8 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[int]
         """
+        if not root:
+            return []
         queue = deque()
         queue.append(root)
         ans = []
