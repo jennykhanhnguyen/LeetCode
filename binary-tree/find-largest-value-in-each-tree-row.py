c9302adb54afcb23ac2efef3352cfdf2f0e2f88d
Jenny Nguyen <nguyen_j6@denison.edu>
2026-01-19 18:27:23 -0500

    LC-find-largest-value-in-each-tree-row [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/find-largest-value-in-each-tree-row.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution(object):
+    def largestValues(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        queue = deque()
+        queue.append(root)
+        ans = []
+        while queue:
+            size = len(queue)
+            maxval = -1
+            for i in range(size):
+                node = queue.popleft()
+                maxval = max(maxval, node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            ans.append(maxval)
+        return ans
