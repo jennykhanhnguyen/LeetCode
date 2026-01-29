5291e062f5dcf73488afea6f8d0510caa3b51189
Jenny Nguyen <nguyen_j6@denison.edu>
2026-01-29 16:23:45 -0500

    LC-accounts-merge [Runtime: N/A; Memory: N/A]

--- depth-first-search/accounts-merge.py
+++ depth-first-search/accounts-merge.py
@@ -1,58 +1,74 @@
-from collections import defaultdict, deque
-
+from collections import defaultdict
 class Solution(object):
     def accountsMerge(self, accounts):
         """
         :type accounts: List[List[str]]
         :rtype: List[List[str]]
         """
-        graph = defaultdict(set)
-        email_to_name = {}
+        def find(x):
+            if x == rep[x]:
+                return x
+            else:
+                rep[x] = find(rep[x])
+                return rep[x]
+        def combine(u,v):
+            u = find(u)
+            v = find(v)
+            if u == v:
+                return
+            else:
+                if size[u] > size[v]:
+                    rep[v] = u
+                    size[u] += size[v]
+                else:
+                    rep[u] = v
+                    size[v] += size[u]
+        rep = defaultdict(str)
+        size = defaultdict(int)
+        name = defaultdict(str)
+        for i, lsti in enumerate(accounts):
+            for k in range(1,len(lsti)):
+                rep[accounts[i][k]] = accounts[i][1]
+                size[accounts[i][k]] = 1
+                name[accounts[i][k]] = accounts[i][0]
 
-        # 1) Build the graph
-        for acc in accounts:
-            name = acc[0]
-            emails = acc[1:]
+        # print(rep)
 
-            # Record name for each email
-            for e in emails:
-                email_to_name[e] = name
+        # set for each list
+        for i, lsti in enumerate(accounts):
+            if i+1 <= len(accounts):
+                for lstj in accounts[i+1:]:
+                    if lsti[0] == lstj[0]:
+                        # print(lsti,lstj)
+                        setlstj = set(lstj[1:])
+                        # print(setlstj)
+                        for k in range(1, len(lsti)):
+                            if lsti[k] in setlstj:
+                                elmt = setlstj.pop()
+                                while lsti[k] == elmt:
+                                    elmt = setlstj.pop()    
+                                # print(lsti[k], elmt)                    
+                                combine(lsti[k], elmt)
 
-            # Connect all emails in this account together
-            first = emails[0]
-            for e in emails[1:]:
-                graph[first].add(e)
-                graph[e].add(first)
+        ans = defaultdict(list)
+        anslst = []
+        for key, val in rep.items(): # key: email, val: rep
+            ans[find(key)].append(key)
 
-            if first not in graph:
-                graph[first] = set()
+        for key, vallst in ans.items():
+            k = name[key]
+            each = []           
+            for item in vallst:
+                each.append(item)
+            each.sort()
+            each = [k]+each
+            anslst.append(each)
+        # print(rep)
+        # print(ans)
+        # print(anslst)
+        
+        return anslst
+                
 
 
-        # 2) BFS to find connected components of emails
-        visited = set()
-        res = []
-
-        for email in graph:
-            if email in visited:
-                continue
-
-            # Start BFS for this component
-            q = deque([email])
-            visited.add(email)
-            component_emails = []
-
-            while q:
-                cur = q.popleft()
-                component_emails.append(cur)
-
-                for nei in graph[cur]:
-                    if nei not in visited:
-                        visited.add(nei)
-                        q.append(nei)
-
-            # 3) Format output: [name] + sorted emails
-            component_emails.sort()
-            name = email_to_name[email]  # any email in component works
-            res.append([name] + component_emails)
-
-        return res
+        
