db886db052282b28deca3d4bce9768bc07a298ae
Jenny Nguyen <nguyen_j6@denison.edu>
2026-02-17 14:00:43 -0500

    LC-redundant-connection [Runtime: 0 ms; Memory: 19.8 MB]

--- graph/redundant-connection.py
+++ graph/redundant-connection.py
@@ -1,32 +1,32 @@
-from collections import defaultdict, deque
-
 class Solution:
-    def findRedundantConnection(self, edges):
-        graph = defaultdict(list)              # adjacency list
-        ans = None                             # store last redundant edge
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        rep = []
+        for i in range(len(edges)):
+            rep.append(i)
+        size = [1]*len(edges)
 
-        def has_path_dfs(src, target):
-            stack = [src]                      # DFS stack
-            seen = set([src])                  # visited set
-
-            while stack:                       # standard DFS loop
-                node = stack.pop()
-                if node == target:             # found a path src -> target
-                    return True
-                for nei in graph[node]:        # explore neighbors
-                    if nei not in seen:
-                        seen.add(nei)
-                        stack.append(nei)
-            return False                        # no path exists
-
-        for u, v in edges:                     # process edges in given order
-            # If u and v are already connected, this edge creates a cycle
-            if u in graph and v in graph and has_path_dfs(u, v):
-                ans = [u, v]                   # record it (keep last one)
+        def find(u):
+            if u == rep[u]:
+                return u
             else:
-                graph[u].append(v)             # add edge to graph
-                graph[v].append(u)
+                rep[u] = find(rep[u])
+                return rep[u]
+        def combine(u,v):
+            u = find(u)
+            v = find(v)
+            if u == v:
+                return True
+            else:
+                if size[u] > size[v]:
+                    rep[v] = u
+                    size[u] += size[v]
+                else:
+                    rep[u] = v
+                    size[v] += size[u]
+            return False
 
-        return ans
-
-        
+        for left, right in edges:
+            redundant = combine(left-1, right-1)
+            if redundant == True:
+                return [left, right]
+        return []
