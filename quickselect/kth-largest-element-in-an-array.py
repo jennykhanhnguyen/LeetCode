274acc7b71b4cae36969b6b439e9a21b7711ff2c
Jenny Nguyen <nguyen_j6@denison.edu>
2026-02-23 15:50:26 -0500

    LC-kth-largest-element-in-an-array [Runtime: 662 ms; Memory: 20 MB]

---
+++ quickselect/kth-largest-element-in-an-array.py
@@ -0,0 +1,14 @@
+import heapq
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        heap = []
+        for i in range(len(nums)):
+            heapq.heappush(heap, nums[i])
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return heap[0]
