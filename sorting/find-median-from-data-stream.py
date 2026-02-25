9392ce736c43c1ab8a488ac0960b1fbaee0b5a19
Jenny Nguyen <nguyen_j6@denison.edu>
2026-02-24 20:42:19 -0500

    LC-find-median-from-data-stream [Runtime: 1144 ms; Memory: 36.1 MB]

---
+++ sorting/find-median-from-data-stream.py
@@ -0,0 +1,31 @@
+import heapq
+class MedianFinder(object):
+
+    def __init__(self):
+        self.min_heap = [] 
+        self.max_heap = []    
+
+    def addNum(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        heapq.heappush(self.max_heap, -num)
+        heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+
+        if len(self.min_heap) > len(self.max_heap):
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+
+    def findMedian(self):
+        """
+        :rtype: float
+        """
+        if len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        else:
+            return (-self.max_heap[0] + self.min_heap[0]) / 2.0
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
