b1aaf32acf9dc1db0afa3d6c4459e72e1692be8f
Jenny Nguyen <nguyen_j6@denison.edu>
2026-02-25 13:36:10 -0500

    LC-koko-eating-bananas [Runtime: 204 ms; Memory: 20.7 MB]

--- array/koko-eating-bananas.py
+++ array/koko-eating-bananas.py
@@ -1,23 +1,21 @@
-class Solution(object):
-    def minEatingSpeed(self, piles, h):
-        """
-        :type piles: List[int]
-        :type h: int
-        :rtype: int
-        """
-        def can_finish_eating(piles, h, k):
-            hours_used = 0
-            for p in piles:
-                hours_used += ceil(float(p)/k)
-            return hours_used <= h
-
-        left, right = 1, 1000000000 # 10^9 max pile size
-        ans = -1
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def can_finish(k):
+            total = 0
+            for i in range(len(piles)):
+                total += math.ceil(piles[i]/k)
+            if h >= total:
+                return True
+            return False
+        left = 1
+        right = max(piles)
         while left <= right:
-            mid = (left + right) // 2
-            if can_finish_eating(piles, h, mid):
-                ans = mid
-                right = mid - 1
+            mid = (left + right) // 2            
+            if can_finish(mid):
+                answer = mid        
+                right = mid - 1     
             else:
-                left = mid + 1
-        return ans
+                left = mid + 1      
+        
+        return answer
