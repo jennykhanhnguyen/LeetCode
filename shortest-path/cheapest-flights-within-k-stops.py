357fa1764a7b007409d57e3d22010cd13c83bf3d
Jenny Nguyen <nguyen_j6@denison.edu>
2026-03-01 01:22:28 -0500

    LC-cheapest-flights-within-k-stops [Runtime: N/A; Memory: N/A]

---
+++ shortest-path/cheapest-flights-within-k-stops.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        graph = defaultdict(list)
+        for l, r, w in flights:
+            graph[l].append((r,w))
+        total_price = float("inf")
+        visited = set()
+        visited.add(src)
+        def dfs(node, price, stop):
+            nonlocal total_price
+            if stop > k +1:
+                return
+            elif node == dst:
+                total_price = min(total_price, price)
+                return 
+            for nei, wei in graph[node]:
+                if nei not in visited and price + wei < total_price:
+                    visited.add(nei)
+                    dfs(nei, price + wei, stop + 1)
+                    visited.remove(nei)                
+        dfs(src, 0, 0)
+        return total_price if total_price != float("inf") else -1
