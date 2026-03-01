26cca03fc64190d3fd69ac28fffee34538df6358
Jenny Nguyen <nguyen_j6@denison.edu>
2026-03-01 17:22:22 -0500

    LC-minimum-size-subarray-sum [Runtime: 47 ms; Memory: 30.5 MB]

--- array/minimum-size-subarray-sum.py
+++ array/minimum-size-subarray-sum.py
@@ -3,19 +3,19 @@ import bisect
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         n = len(nums)
-        
+
         prefix = [0] * (n + 1)
         for i in range(n):
             prefix[i+1] = prefix[i] + nums[i]
-        
+
         ans = float('inf')
-        
+
         for j in range(1, n + 1):
             needed = prefix[j] - target
-            
-            i = bisect.bisect_left(prefix, needed)
-            
-            if i < j and prefix[i] <= needed:
+
+            i = bisect.bisect_right(prefix, needed, 0, j) - 1
+
+            if i >= 0:
                 ans = min(ans, j - i)
-        
+
         return ans if ans != float('inf') else 0
