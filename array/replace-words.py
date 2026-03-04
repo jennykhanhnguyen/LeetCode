4b276bacd1ae88be54fc24100374867ba92a99bb
Jenny Nguyen <nguyen_j6@denison.edu>
2026-03-04 12:11:12 -0500

    LC-replace-words [Runtime: N/A; Memory: N/A]

---
+++ array/replace-words.py
@@ -0,0 +1,44 @@
+class Solution:
+    class Node:
+        def __init__(self):
+            self.children = {}
+            self.is_end = False
+    def __init__(self): 
+        self.root = self.Node()
+    def add(self, word):
+        current = self.root
+        for i in range(len(word)):
+            c = word[i]
+            if c not in current.children:
+                current.children[c] = self.Node()
+            current = current.children[c]
+        current.is_end = True
+
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        dct = Solution()
+        ans = []
+        for word in dictionary:
+            dct.add(word)
+        lst = sentence.split(" ")
+        for word in lst:
+            current = dct.root
+            root = ''
+            for i in range(len(word)):
+                c = word[i]
+                root += c
+                if c in current.children:
+                    if current.children[c].is_end == True:
+                        ans.append(root)
+                        break
+                    else:
+                        current = current.children[c]
+                else:
+                    ans.append(word)
+                    break
+        ans_string = " ".join(ans)
+        return ans_string
+        
+                
+
+
+        
