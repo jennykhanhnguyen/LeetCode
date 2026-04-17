be703d46dcc8cf4223c8a02f51e0018ca882c84c
Jenny Nguyen <nguyen_j6@denison.edu>
2026-04-16 23:05:59 -0400

    LC-non-overlapping-intervals [Runtime: N/A; Memory: N/A]

--- dynamic-programming/non-overlapping-intervals.py
+++ dynamic-programming/non-overlapping-intervals.py
@@ -2,6 +2,7 @@ class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         ans = 1
         intervals.sort(key=lambda x: x[1])
+        # print(intervals)
         current_start = intervals[0][0]
         current_end = intervals[0][1]
         for index, item in enumerate(intervals):
@@ -14,5 +15,7 @@ class Solution:
                 continue
             if start >= current_start and start < current_end:
                 continue
+            # print(current_start, current_end, start, end)
+            current_end = end
             ans += 1
         return len(intervals) - ans
