ad594630c573ec2df5d453fceaac779ccc74a9a3
Jenny Nguyen <nguyen_j6@denison.edu>
2026-04-23 15:01:39 -0400

    LC-insert-delete-getrandom-o1-duplicates-allowed [Runtime: N/A; Memory: N/A]

---
+++ math/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+import random 
+class RandomizedCollection:
+
+    def __init__(self):
+        self.lst = []
+        self.hm = defaultdict(set)
+
+    def insert(self, val: int) -> bool:
+        boolean = True
+        if val in self.hm:
+            boolean = False
+        self.lst.append(val)
+        self.hm[val].add(len(self.lst)-1)
+        return boolean
+
+    def remove(self, val: int) -> bool:
+        if val not in self.hm or len(self.hm[val]) == 0:
+            return False
+        index_rm = self.hm[val].pop()
+        last_num = self.lst[-1] 
+        index_last_num = len(self.lst) -1
+        self.lst[-1], self.lst[index_rm] = self.lst[index_rm],self.lst[-1]
+        self.hm[last_num].remove(index_last_num)
+        self.hm[last_num].add(index_rm)
+        self.lst.pop()
+        return True
+
+    def getRandom(self) -> int:
+        rand = random.randint(0, len(self.lst)-1)
+        return self.lst[rand]
+
+
+# Your RandomizedCollection object will be instantiated and called as such:
+# obj = RandomizedCollection()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
