5ea10ffc04f8c8e7849db1816d54e070e71c793f
Jenny Nguyen <nguyen_j6@denison.edu>
2026-04-23 15:26:07 -0400

    LC-insert-delete-getrandom-o1-duplicates-allowed [Runtime: N/A; Memory: N/A]

--- randomized/insert-delete-getrandom-o1-duplicates-allowed.py
+++ randomized/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -12,18 +12,20 @@ class RandomizedCollection:
             boolean = False
         self.lst.append(val)
         self.hm[val].add(len(self.lst)-1)
+        print(val, self.hm)
         return boolean
 
     def remove(self, val: int) -> bool:
-        if val not in self.hm or len(self.hm[val]) == 0:
+        if val not in self.hm or len(self.hm[val]) == 0 or len(self.lst) == 0:
             return False
         index_rm = self.hm[val].pop()
         last_num = self.lst[-1] 
         index_last_num = len(self.lst) -1
         self.lst[-1], self.lst[index_rm] = self.lst[index_rm],self.lst[-1]
-        self.hm[last_num].remove(index_last_num)
-        self.hm[last_num].add(index_rm)
-        self.lst.pop()
+        print(self.hm[last_num])
+        # self.hm[last_num].remove(index_last_num)
+        # self.hm[last_num].add(index_rm)
+        # self.lst.pop()
         return True
 
     def getRandom(self) -> int:
